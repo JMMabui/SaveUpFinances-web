29a9ac7a729584282bd6fff93b3470ed8d61c5f0
JMMabui <justinsalomon2@gmail.com>
2025-07-25 15:39:53 +0200

    chore: update dependencies and improve Vite configuration
    - Added @tailwindcss/vite and tailwindcss to dependencies in package.json
    - Added @types/node to devDependencies in package.json
    - Updated import statement for path in vite.config.ts to use node:path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // biome-ignore lint/style/useNodejsImportProtocol: <explanation>
-import path from 'path'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
